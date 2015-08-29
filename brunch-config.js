4eed5c1c4b86f7145444025e60ffa36d5aa16889
Louis Pilfold <louis@lpil.uk>
2015-08-29 17:54:26 +0100

    We like SCSS more than CSS

--- brunch-config.js
+++ brunch-config.js
@@ -3,10 +3,19 @@
 exports.config = {
   files: {
     javascripts: {
-      joinTo: {
-        'main.js': /^app/,
-      }
+      joinTo: 'main.js',
     },
+    stylesheets: {
+      joinTo: 'main.css',
+      defaultExtension: 'scss',
+    },
+  },
+
+  paths: {
+    watched: [
+      'app/',
+      'stylesheets/',
+    ],
   },
 
   npm: { enabled: true },
